3aa0130e9dc32c0352cb48cae3a039db1a253997
lingzili <lingzili@bmb.sdu.dk>
2019-03-29 23:20:04 +0100

    R solution for DNA

---
+++ DNA.R
@@ -0,0 +1,11 @@
+# Open dna file
+dna <- readChar("~/Rosalind_Python/rosalind_dna.txt", nchars = file.info("~/Rosalind_Python/rosalind_dna.txt")$size)
+
+# Check structure of the file dna
+str(dna)
+
+# Load library stringr
+library(stringr)
+
+# Count A, C, G, T in dna
+str_count(dna, c("A","C","G","T"))
