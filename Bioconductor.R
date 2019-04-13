a2e243f4b7b85cd0990c92d053d521a60773ee2b
lingzili <lingzili@bmb.sdu.dk>
2019-04-13 23:20:23 +0200

    try out more bioconductor functions

--- Bioconductor.R
+++ Bioconductor.R
@@ -1,3 +1,6 @@
+# Print version information about R, the OS and attached or loaded packages
+sessionInfo()
+
 # Check if your installation is fully up-to-date
 biocValid()
 
@@ -10,3 +13,14 @@ genome <- BSgenome.Mmusculus.UCSC.mm10
 seqlengths(genome)
 
 genome$chr11
+
+class(genome)
+
+# Check out package Biostrings
+library(Biostrings)
+
+dna <- DNAStringSet(c("AACAT", "GGCGCCT"))
+
+reverseComplement(dna)
+
+letterFrequency(genome$chr11, "GC", as.prob = TRUE) # Took a while to run!!
